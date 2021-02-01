b32d66298d4a717a05c574c820b253abd28c03b7
theophanee <77969774+theophanee@users.noreply.github.com>
2021-02-01 19:00:36 +0100

    Update Static.py

--- src/Static.py
+++ src/Static.py
@@ -45,7 +45,7 @@ class Static:
                 print("%-3s"%("-"),end="")
             print("|")
         
-        
+     @staticmethod
      def add_Objet(self, objet, x, y):
         """Assuming objet is type Objet"""
         if ( 0 <= x < self.larg ) and ( 0 <= y < self.long ) and ( self.is_Occupe(x,y) == False ) :
@@ -53,5 +53,6 @@ class Static:
             return self.is_Occupe( x, y)
         return False
 
+    @staticmethod
     def is_Occupe(self, x, y):
         return self.grille[x][y]!=None
