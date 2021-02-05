b8eab7913dc6139c99a321c84c59b1537301b4d1
theophanee <77969774+theophanee@users.noreply.github.com>
2021-02-05 14:32:29 +0100

    Update Simulation.py

--- src/Simulation.py
+++ src/Simulation.py
@@ -6,26 +6,30 @@ from Static import Static
 
 class Simulation:
 
+    #Constructeur
     def __init__(self, largeur, longueur, echelle, vision, taille_robot):
+        # Crée la grille de simulation en fonction des parametres donnés avec l'échelle, le robot, la largeur et la longueur de la simulation
         self.larg = largeur*echelle.nbCases
         self.long = longueur*echelle.nbCases
         self.grille = Static.createGrille(self.larg, self.long)
-        self.__init_wall_grille()
+        self.__init_wall_grille() #pose des robots sur les bornes de la grille
         self.vision = vision
         self.taille_robot = taille_robot
+        #crée le robot de la simulation
         self.robotSimu = RobotSimu()
         self.init_Robot()
         Static.affiche(self.grille)
 
-        
+    #crée le robot de la simulation
     def init_Robot(self):
-        self.robotSimu.setPos(int(self.larg / 2), int(self.long / 2), 0)
+        self.robotSimu.setPos(int(self.larg / 2), int(self.long / 2), 0) #le pose en plein milieu du terrain
         pair = self.taille_robot % 2
+        # pose le robot sur certaines cases en fonction de l'échelle et sa taille
         for i in range(self.robotSimu.posx - int(self.taille_robot/2) , self.robotSimu.posx + int(self.taille_robot/2) + pair):
             for j in range( self.robotSimu.posy - int(self.taille_robot/2) , self.robotSimu.posy + int(self.taille_robot/2) + pair):
                 self.grille[i][j] = self.robotSimu
 
-
+    #positionne des murs sur les limites du terrain
     def __init_wall_grille(self):
     	for i in range(len(self.grille)):
     		self.grille[i][0] = Wall()
@@ -35,6 +39,6 @@ class Simulation:
     		self.grille[0][i] = Wall()
     		self.grille[len(self.grille)-1][i] = Wall()
 
-    
+    #avance le robot
     def forward(self, x, speed):
         return True
