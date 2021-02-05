d6d144b1567a24e03acf64a1251bf940b0cfcaa1
theophanee <77969774+theophanee@users.noreply.github.com>
2021-02-05 15:18:02 +0100

    Update Vision.test.py

--- src/Vision.test.py
+++ src/Vision.test.py
@@ -2,15 +2,16 @@ from Vision import Vision
 from Static import Static
 from Wall import Wall
 
+#test le fonctionnement de la vision
 def testAdd_Objet(vision):
-
+    #ajoute des Wall aux à certaines positions
     print(Static.add_Objet(vision.grille,Wall(),2,3))
     print(Static.add_Objet(vision.grille,Wall(),2,3))
     print(Static.add_Objet(vision.grille,Wall(),3,2))
 
 
 if __name__ == '__main__':
-    v = Vision(10, 10)
+    v = Vision(10, 10) #grille de 10*10
     try:
         testAdd_Objet(v)
         Static.affiche(v.grille)
