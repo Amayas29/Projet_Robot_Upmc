cc17605b6ade885b53a3836da851f1ae7d7521ee
theophanee <77969774+theophanee@users.noreply.github.com>
2021-02-05 15:16:01 +0100

    Update Vision.py

--- src/Vision.py
+++ src/Vision.py
@@ -1,14 +1,17 @@
 # from Objet import Objet
 from Static import Static
 
+#représente la vision du robot, ce qu'il voit devant lui
 class Vision:
 
+    #Constructeur
     def __init__(self, larg, long, tailleRob):
+        #crée une mini grille,copiant sur celle de simulation
         self.grille = Static.createGrille(larg, long)
         self.larg = larg
         self.long = long
         self.tailleRob = tailleRob
 
-    
+    #Determine si sur une distance donnee, il y a des obstacles ou non
     def libresur(self, x):
         return True
