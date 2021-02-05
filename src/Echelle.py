a8ffcfd063ba9731d56c17964e54c57b32731e35
theophanee <77969774+theophanee@users.noreply.github.com>
2021-02-05 14:11:50 +0100

    Update Echelle.py

--- src/Echelle.py
+++ src/Echelle.py
@@ -1,6 +1,8 @@
 # from Objet import Objet
 
+# Classe qui se charge d'établir une échelle pour représenté la simulation
 class Echelle:
 
     def __init__(self, nbCases):
+        #attribue tel nombre de cases pour une échelle donnée
         self.nbCases = nbCases
