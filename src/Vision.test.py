7587b0769e361d2348d7baa866bb88abbc3235f9
Amayas29 <amayas29sd@gmail.com>
2021-02-06 16:10:46 +0100

    Refactor code

--- src/Vision.test.py
+++ src/Vision.test.py
@@ -1,21 +1,21 @@
 from Vision import Vision
-from Static import Static
+from Tool import *
 from Wall import Wall
 
 #test le fonctionnement de la vision
 def testAdd_Objet(vision):
     #ajoute des Wall aux à certaines positions
-    print(Static.add_Objet(vision.grille,Wall(),2,3))
-    print(Static.add_Objet(vision.grille,Wall(),2,3))
-    print(Static.add_Objet(vision.grille,Wall(),3,2))
+    print(add_Objet(vision.grille,Wall(),2,3))
+    print(add_Objet(vision.grille,Wall(),2,3))
+    print(add_Objet(vision.grille,Wall(),3,2))
 
 
 if __name__ == '__main__':
-    v = Vision(10, 10) #grille de 10*10
+    v = Vision(10, 10, 1) #grille de 10*10
     try:
         #essaye de
         testAdd_Objet(v) # ajouter les murs
-        Static.affiche(v.grille) #affiche la vision
+        affiche(v.grille) #affiche la vision
         print("\nTest: Add Objet successful")
 
     except AssertionError as e:
