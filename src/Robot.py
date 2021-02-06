91038749fef634762b715c1518e929ed0420f145
Amayas29 <amayas29sd@gmail.com>
2021-02-07 00:58:48 +0100

    Changement des dimensions pour tests

--- src/Robot.py
+++ src/Robot.py
@@ -35,7 +35,7 @@ class Robot:
             print("Mode Simulation active")
             #on initialise la simulation
             self.isSimu = True
-            self.simu = Simulation(5,5,self.echelle, self.vision, self.tailleRobot)
+            self.simu = Simulation(10, 10,self.echelle, self.vision, self.tailleRobot)
         else:
             #sinon on parametre le robot pour l'utiliser irl
             print("Mode IRL active")
