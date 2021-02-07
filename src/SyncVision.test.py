759936a0cfa59b04ec04ff205ed4db3bd70eda13
Amayas29 <amayas29sd@gmail.com>
2021-02-07 07:43:20 +0100

    Test de SyncVision (TODO : + Precision et enlever ligne robot + selection extermite)

--- src/SyncVision.test.py
+++ src/SyncVision.test.py
@@ -17,11 +17,23 @@ add_Objet(sim.grille, Objet(), 30, 22)
 add_Objet(sim.grille, Objet(), 30, 21)
 add_Objet(sim.grille, Objet(), 30, 27)
 add_Objet(sim.grille, Objet(), 30, 26)
+add_Objet(sim.grille, Objet(), 29, 22)
+add_Objet(sim.grille, Objet(), 29, 21)
+add_Objet(sim.grille, Objet(), 29, 27)
+add_Objet(sim.grille, Objet(), 29, 26)
+add_Objet(sim.grille, Objet(), 28, 26)
+add_Objet(sim.grille, Objet(), 31, 27)
+add_Objet(sim.grille, Objet(), 27, 26)
+
+# Attention ! Le robot est encore dans la vision 
+# faut encore plus de precision et enlever la ligne du robot
+# Pour voir ça : 
+# add_Objet(sim.grille, Objet(), 25, 26)
 
 affiche(sim.grille)
 
 print("\n\n")
-sim.robotSimu.direction = 00
+sim.robotSimu.direction = 0
 
 print("Angle ", sim.robotSimu.direction, "°\n\n")
 
