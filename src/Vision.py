ae2afc341caf5485508aebc7410207ba9e234c49
HamidKolli <gamog5756@gmail.com>
2021-02-07 21:05:30 +0100

    test et modification de libresur

--- src/Vision.py
+++ src/Vision.py
@@ -14,17 +14,27 @@ class Vision:
 
     #Determine si sur une distance donnee, il y a des obstacles ou non
     def libresur(self, x):
-        if(x <= len(self.grille[0])):
-            for i in range(0,x): #Boucle parcourant la distance de 0 jusqu'à x 
-                for j in range(0,self.tailleRob): #Boucle parcourant la taille du robot
-                    if (is_Occupe(self.grille,j,self.long-i)): #Regarde les cases remontant a partir du coin inférieur gauche grace a self.long - i
+        # if(x <= len(self.grille[0])):
+        #     for i in range(0,x): #Boucle parcourant la distance de 0 jusqu'à x 
+        #         for j in range(0,self.tailleRob): #Boucle parcourant la taille du robot
+        #             if (is_Occupe(self.grille,j,self.long-i)): #Regarde les cases remontant a partir du coin inférieur gauche grace a self.long - i
+        #                 print("impossible il y a un objet !")
+        #                 return False
+        #     print("On peut avancer !")
+        #     return True
+        # else:
+        #     print("Vision impossible !")
+        if(x > self.larg):
+            print("Vision impossible !")
+            return
+        
+        for i in range(self.long//2 - self.tailleRob//2,self.long//2 + self.tailleRob//2):
+            for j in range(x):
+                if (is_Occupe(self.grille,j,i)): #Regarde les cases remontant a partir du coin inférieur gauche grace a self.long - i
                         print("impossible il y a un objet !")
                         return False
-            print("On peut avancer !")
-            return True
-        else:
-            print("Vision impossible !")
-
+        print("On peut avancer !")
+        return True
     
     def distanceMaxObstacle(self):
         #return la distance max avant le premier obstacle rencontrer (en prenant en compte la largeur du robot)
