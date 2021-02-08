6c1352933f754e4c3eea1518f866378c196d24cc
Amayas29 <amayas29sd@gmail.com>
2021-02-08 15:31:52 +0100

    Test pour nouvelle vision

--- src/sync_vision_test.py
+++ src/sync_vision_test.py
@@ -6,8 +6,9 @@ import random
 
 rob = Robot(8)
 rob.set_simu(True)
-sim = rob.simu
-
+sim = Simulation(5, 5,rob.echelle, rob.vision, rob.taille_robot)
+rob.simu = sim
+random.seed(30)
 for i in range(len(sim.grille)):
    for j in range(len(sim.grille[0])):
        if str(sim.grille[i][j]) != "RR":
@@ -21,47 +22,50 @@ for i in range(len(sim.grille)):
 # print( "\n\nPosition robot :\nx = " + str(x) + " , y = " + str(y) )
 
 # sim.grille[x][y] = rob.simu.robot_simu
-# rob.simu.robot_simu.setPos(x, y, 0)
+# rob.simu.robot_simu.set_pos(x, y, 0)
 
 print("\n\n")
 
-# add_objet(sim.grille, Objet(), 30, 25)
-# add_objet(sim.grille, Objet(), 30, 24)
-# add_objet(sim.grille, Objet(), 30, 23)
-# add_objet(sim.grille, Objet(), 30, 22)
-# add_objet(sim.grille, Objet(), 30, 21)
-# add_objet(sim.grille, Objet(), 30, 27)
-# add_objet(sim.grille, Objet(), 30, 26)
-# add_objet(sim.grille, Objet(), 29, 22)
-# add_objet(sim.grille, Objet(), 29, 21)
-# add_objet(sim.grille, Objet(), 29, 27)
-# add_objet(sim.grille, Objet(), 29, 26)
-# add_objet(sim.grille, Objet(), 28, 26)
-# add_objet(sim.grille, Objet(), 31, 27)
-# add_objet(sim.grille, Objet(), 27, 26)
+# add_objet_(sim.grille, Objet(), 30, 25)
+# add_objet_(sim.grille, Objet(), 30, 24)
+# add_objet_(sim.grille, Objet(), 30, 23)
+# add_objet_(sim.grille, Objet(), 30, 22)
+# add_objet_(sim.grille, Objet(), 30, 21)
+# add_objet_(sim.grille, Objet(), 30, 27)
+# add_objet_(sim.grille, Objet(), 30, 26)
+# add_objet_(sim.grille, Objet(), 29, 22)
+# add_objet_(sim.grille, Objet(), 29, 21)
+# add_objet_(sim.grille, Objet(), 29, 27)
+# add_objet_(sim.grille, Objet(), 29, 26)
+# add_objet_(sim.grille, Objet(), 28, 26)
+# add_objet_(sim.grille, Objet(), 31, 27)
+# add_objet_(sim.grille, Objet(), 27, 26)
 
-add_objet(sim.grille,'A', 21, 20)
-add_objet(sim.grille, 'B', 21, 21)
-add_objet(sim.grille,'C', 21, 22)
-add_objet(sim.grille, 'D', 21, 19)
-add_objet(sim.grille, 'E', 21, 18)
-add_objet(sim.grille, "F", 21, 22)
-add_objet(sim.grille,'G', 21, 17)
-add_objet(sim.grille, 'H', 21, 16)
-add_objet(sim.grille,'I', 21, 23)
-add_objet(sim.grille, 'K', 21, 24)
-add_objet(sim.grille, 'L', 21, 15)
-add_objet(sim.grille, "M", 21, 25)
-add_objet(sim.grille,'N', 21, 14)
-add_objet(sim.grille, 'O', 21, 13)
-add_objet(sim.grille, 'P', 21, 26)
-add_objet(sim.grille, "Q", 21, 27)
-add_objet(sim.grille, "T", 21, 28)
-add_objet(sim.grille, "S", 21, 12)
+add_objet_(sim.grille,'A', 21, 20)
+add_objet_(sim.grille, 'B', 21, 21)
+add_objet_(sim.grille,'C', 21, 22)
+add_objet_(sim.grille, 'D', 21, 19)
+add_objet_(sim.grille, 'E', 21, 18)
+add_objet_(sim.grille, "F", 21, 22)
+add_objet_(sim.grille,'G', 21, 17)
+add_objet_(sim.grille, 'H', 21, 16)
+add_objet_(sim.grille,'I', 21, 23)
+add_objet_(sim.grille, 'K', 21, 24)
+add_objet_(sim.grille, 'L', 21, 15)
+add_objet_(sim.grille, "M", 21, 25)
+add_objet_(sim.grille,'N', 21, 14)
+add_objet_(sim.grille, 'O', 21, 13)
+add_objet_(sim.grille, 'P', 21, 26)
+add_objet_(sim.grille, "Q", 21, 27)
+add_objet_(sim.grille, "T", 21, 28)
+add_objet_(sim.grille, "S", 21, 12)
 
-add_objet(sim.grille,'$', 36, 20)
+add_objet_(sim.grille,'$', 36, 20)
 
-add_objet(sim.grille,'£', 30, 20)
+add_objet_(sim.grille,'£', 30, 20)
+
+add_objet_(sim.grille,'7', 20, 21)
+add_objet_(sim.grille,'1', 19, 21)
 
 affiche(sim.grille)
 
