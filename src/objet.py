c3019cda18cac893c8c21716bb2b64009ad4e7f2
Amayas29 <amayas29sd@gmail.com>
2021-02-08 15:29:02 +0100

    Ajout de NotDefined pour les zones non accessibles par vision

--- src/objet.py
+++ src/objet.py
@@ -13,3 +13,10 @@ class Objet:
         # Juste si on a pas d'objet specifique et qu'on utilise directement le objet on aura des O
         # A voir si on le supprime ou pas ?
         return "O"
+
+
+# Qui represenete les cases non definie dans la vision (A discuter avec les autres pour voir leurs avis)
+class NotDefined(Objet):
+
+    def __str__(self):
+        return "ND"
