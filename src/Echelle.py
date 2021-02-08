46b45293338b766542df15ddb9f0449debd91565
Amayas29 <amayas29sd@gmail.com>
2021-02-08 12:13:59 +0100

    Refactor code (Convention)

--- src/Echelle.py
+++
@@ -1,7 +0,0 @@
-
-# Classe qui se charge d'établir une échelle pour représenté la simulation
-class Echelle:
-
-    def __init__(self, nbCases):
-        #attribue tel nombre de cases pour une échelle donnée
-        self.nbCases = nbCases
