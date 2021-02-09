f49f90a0c0e1dac9e923eca04524fc05e97dc857
theophanee <theophane@LAPTOP-VTGK9NBU.localdomain>
2021-02-09 15:02:26 +0100

    commentaires

--- src/deplacement.test.py
+++ src/deplacement.test.py
@@ -2,6 +2,8 @@ from robot import *
 from simulation import *
 from tool import *
 
+#test la méthode forward qui permet de déplacer le robot
+
 # rob = Robot(8) Pas encore
 rob = Robot(4)
 rob.set_simu(True)
@@ -9,7 +11,7 @@ rob.set_simu(True)
 # rob.simu.forward(50, 0) Pas encore
 # rob.simu.forward(2, 20) Pas encore
 
-# Explication de ce test ? il avance de 18 cases
+# il avance de 18 cases
 affiche(rob.simu.grille)
 
 rob.simu.forward(2, 20)
