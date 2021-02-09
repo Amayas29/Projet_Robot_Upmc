1cbef4396b3a1f9d6edc9dead26e13cea0957820
theophanee <theophane@LAPTOP-VTGK9NBU.localdomain>
2021-02-09 14:55:52 +0100

    commentaires

--- src/libre_sur.test.py
+++ src/libre_sur.test.py
@@ -2,6 +2,7 @@ from vision import Vision
 from tool import *
 from wall import Wall
 
+#tests de la nouvelle méthode libresur
 vis = Vision(9*4,9*4,int(9*0.25))
 
 # for i in range(len(vis.grille)):
@@ -16,7 +17,7 @@ for i in range(len(vis.grille)):
     print(i)
     vis.grille[3][i] = None
 
-
+#parcours une grille et test si les cases sont libres
 
 for i in range(vis.long//2 - vis.taille_rob//2,vis.long//2 + vis.taille_rob//2):
     vis.grille[i][0] = "R"
