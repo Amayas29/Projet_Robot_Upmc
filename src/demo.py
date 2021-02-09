66c42d571fb7b09816c055b735c8fc05edafd9e5
theophanee <theophane@LAPTOP-VTGK9NBU.localdomain>
2021-02-09 15:02:45 +0100

    commentaires

--- src/demo.py
+++ src/demo.py
@@ -6,9 +6,13 @@ from random import randint, choice, seed
 from robotsimu import RobotSimu
 from time import sleep
 
+#  DEMO POUR LE CLIENT 10/02/2021
+
 rob = Robot(16)
 rob.set_simu(True)
 seed(70)
+
+#montre la simulation initiale
 
 print("\n - Etat initial : \n")
 
@@ -18,6 +22,8 @@ print("\n - Les coordonnes du robot : (", rob.simu.robot_simu.posy, rob.simu.rob
 
 print("\n\n - Ajout d'un objet à la position (9, 5)")
 
+#montre sue l'on peut ajouter des objets sur la simu
+
 add_objet(rob.simu.grille, Objet(), 9, 5)
 
 print("\n\n - La grille aprés ajout\n\n")
@@ -26,12 +32,15 @@ affiche(rob.simu.grille)
 
 print("\n\n - Génération d'une grille avec des obstacles pour les deplacement \n\n")
 
+#pose des murs 
 for i in range(1, len(rob.simu.grille)-1):
     for j in range(1, len(rob.simu.grille[0])-1):
         if randint(0, 10) == 0 and not isinstance(rob.simu.grille[i][j], RobotSimu):
             rob.simu.grille[i][j] = Wall()
         
 affiche(rob.simu.grille)
+
+#Demo de la vision du robot en fonction d'un angle
 
 print("\n * Affichage de la vision selon un angle (une direction).\n\n \t 0 Est ► \n\n \t 90 sud ▼ \n\n \t 180 Ouest ◄ \n\n \t 270 Nord ▲ \n\n Et pour les autres direction on prend une valeur depuis l'interval")
 
@@ -44,6 +53,8 @@ rob.simu.sync_vision()
 affiche(rob.simu.vision.grille)
 
 rob.simu.vision.libre_sur(1)
+
+#Demo déplacemet du robot ( sans obstacles, juste montré qu'il peut se déplacer comme on veut ), ici il fera un carré
 
 print("\n\n - Deplacement du robot : Dessin d'un carre : \n\n")
 
@@ -73,6 +84,9 @@ print("\n\n\n")
 
 rob = Robot(16)
 rob.set_simu(True)
+
+#Demo detection d'obstacles 
+#place des osbtacles sur la simu 
 
 add_objet(rob.simu.grille, Wall(), 40, 40)
 add_objet(rob.simu.grille, Wall(), 50, 50)
