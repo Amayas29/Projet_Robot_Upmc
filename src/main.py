cdcf9b257e32d0fbed329bd00e833aa38325bbe6
Amayas29 <amayas29sd@gmail.com>
2021-03-10 13:56:41 +0100

    Juste rm l'ancienne proposition dans main. ... fx xd

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ config.read('config.cfg')
 if (float(config['Version']['config_version']) != 0.2):
   print("Config version non conforme")
   exit()
-if (float(config['Robot']['distance_securiter']) < 13.0):
+if (float(config['Robot']['distance_securite']) < 13.0):
   print("Erreur critique: la distance de sécurité est trop faible! (minimum 13.0)")
   exit()
 mode = config['Robot'].getboolean('mode_simu')
@@ -35,15 +35,7 @@ controleur = Controleur(vision, robot, arene, 60)
 thread_affichage = Thread(target=affichage.boucle)
 thread_controleur = Thread(target=controleur.boucle)
 thread_modele = Thread(target=modele.boucle)
-#thread_sync_vision = Thread(target=controleur.thread_sync_vision)
 
-#thread_sync_vision.start()
 thread_controleur.start()
 thread_modele.start()
 thread_affichage.start()
-
-"""while True:
-
-    n = int(input("Donne ta strat mon bro"))
-    controleur.select_startegie(n)
-"""
