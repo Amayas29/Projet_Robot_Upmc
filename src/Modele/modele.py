7241282dfb301c780f4b6373690fee9f5df107c5
WinnMBG <winn77.m@hotmail.com>
2021-03-21 15:21:36 +0100

    Revert "Forward et tourne V1 ( à revoir sans doute git add modele.py)"
    This reverts commit 9144eac7fb6a49314aa8bf4656da2e529854bcfe.

--- src/Modele/modele.py
+++ src/Modele/modele.py
@@ -1,16 +1,12 @@
 from   time import sleep
 import threading
 from Utils.tools import Vecteur
-from Controleur.vision import Vision
-
 
 class Modele:
 
-    def __init__(self, fps, arene, angle):
+    def __init__(self, fps):
         self.fps = fps
-        self.angle = angle
-        self.arene = arene
-        self.robot = arene.robot
+
     
     def boucle(self):
         while True:
@@ -19,54 +15,40 @@ class Modele:
 
 
     def update(self):
-        # syncroniser la vision     
-        Vision.sync_vision(self.robot,[])
-        
-        if Vision.check_collisions():
-            self.stop()
-
-        self.start(vitesse=2)
-
-        if self.angle != 0:
-            self.tourne(self.angle)
-            self.forward()
+        pass
             
 
+        
     def forward(self):
         """
         Permet de bouger le robot d'une case en suivant ca direction
         """
-
-        # normaliser l'angle et calculer le vecteur de direction
-        angle1 = self.angle % 360
-        vict = Vecteur.get_vect_from_angle(angle1)
-        self.robot.vec_deplacement = vict
+        
+        angle = self.angle % 360
+        vict = Vecteur.get_vect_from_angle(angle)
        
-        self.robot.chg += self.robot.vec_deplacement.vect
-        self.robot.chd += self.robot.vec_deplacement.vect
-        self.robot.cbg += self.robot.vec_deplacement.vect
-        self.robot.cbd += self.robot.vec_deplacement.vect
-        self.robot.center += self.robot.vec_deplacement.vect
-    
-
+        self.chg += vict.vict
+        self.chd += vict.vict
+        self.cbg += vict.vict
+        self.cbd += vict.vict
+        self.center += vict.vict
+        
+        
     def start(self, vitesse):
-        self.robot.lspeed = vitesse
-        self.robot.rspeed = vitesse
+        pass
     
     def stop(self):
-        self.robot.lspeed = 0
-        self.robot.rspeed = 0
+        pass
 
     
     def tourne(self, angle):
         """
         Permet de tourner le robot 
         """
-
-        self.angle = angle % 360
-        
-        self.robot.chg.rotation(self.robot.center, angle)
-        self.robot.chd.rotation(self.robot.center,angle)
-        self.robot.cbg.rotation(self.robot.center,angle)
-        self.robot.cbd.rotation(self.robot.center,angle)
+        angle = angle % 360
+        self.angle = angle
+        self.chg.rotation(self.center, angle)
+        self.chd.rotation(self.center,angle)
+        self.cbg.rotation(self.center,angle)
+        self.cbd.rotation(self.center,angle)
       
