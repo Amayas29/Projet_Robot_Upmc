9284956b920d560b87a68190bc5c44ae3d790ad2
Replit user <replituser@example.com>
2021-03-29 10:34:50 +0000

    CHangement tests

--- src/tests.py
+++ src/tests.py
@@ -1,3 +1,14 @@
-from test.modele.test_strategie_avancer import test
+"""
+print("Choisir Strategie : ")
+choix = input("1 : Avancer, 2 : Tourner, 3 : Carre")
 
+if choix==1 :
+  from test.modele.test_strategie_avancer import test
+elif choix==2 :
+  from test.modele.test_strategie_tourner import test
+else : 
+  from test.modele.test_strategie_carre import test
+"""
+
+from test.modele.test_strategie_avancer import test
 test()
