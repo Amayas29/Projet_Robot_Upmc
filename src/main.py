03e63d9939a1a7b65577bda8e4074fda2510050b
Amayas29 <amayas29sd@gmail.com>
2021-03-29 19:00:01 +0200

    Fichier main avec thread OK

--- src/main.py
+++ src/main.py
@@ -1,38 +1,53 @@
 from threading import Thread
 from view.affichage import Affichage
 from controller.controleur import Controleur
+from controller.strategies import Avancer
 from model.robot import Robot
 from model.arene import Arene
-from model.vision import Vision
+from model.obstacles import Obstacle
 from utils.tools import Point
-import configparser
-# protection du config
-config = configparser.ConfigParser()
 
-config.read('config.cfg')
-if (float(config['Version']['config_version']) != 0.2):
-    print("Config version non conforme")
-    exit(1)
-if (float(config['Robot']['distance_securite']) < 13.0):
-    print("Erreur critique: la distance de sécurité est trop faible! (minimum 13.0)")
-    exit(1)
-mode = config['Robot'].getboolean('mode_simu')
-if (mode):
-    print("Simu on")
-else:
-    print("simu off")
+# import configparser
+# # protection du config
+# config = configparser.ConfigParser()
+
+# config.read('config.cfg')
+# if (float(config['Version']['config_version']) != 0.2):
+#     print("Config version non conforme")
+#     exit(1)
+# if (float(config['Robot']['distance_securite']) < 13.0):
+#     print("Erreur critique: la distance de sécurité est trop faible! (minimum 13.0)")
+#     exit(1)
+# mode = config['Robot'].getboolean('mode_simu')
+# if (mode):
+#     print("Simu on")
+# else:
+#     print("simu off")
 # programe
 
-robot = Robot(Point(10, 10), 3, 3, 3)
 arene = Arene()
-vision = Vision(5, 3)
+centre = Point(100, 100)
+robot = Robot(centre, 50, 50, arene)
+arene.set_robot(robot)
 
-affichage = Affichage(60)
-controleur = Controleur(vision, robot, arene, 60)
+src = Point(300, 100)
+dest = Point(800, 100)
 
-thread_affichage = Thread(target=affichage.boucle)
-thread_controleur = Thread(target=controleur.boucle)
-thread_modele = Thread(target=arene.boucle)
+obstacle = Obstacle(src, dest)
+arene.add_obstacle(obstacle)
+
+controleur = Controleur()
+avancer = Avancer(robot, 100000, 200)
+controleur.add_startegie(avancer)
+controleur.select_startegie(0)
+
+affichage = Affichage(arene)
+
+FPS = 60.0
+
+thread_affichage = Thread(target=affichage.boucle, args=(FPS,))
+thread_controleur = Thread(target=controleur.boucle, args=(FPS,))
+thread_modele = Thread(target=arene.boucle, args=(FPS,))
 
 thread_controleur.start()
 thread_modele.start()
