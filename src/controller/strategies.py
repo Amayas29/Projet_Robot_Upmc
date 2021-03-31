2d4aeb34a8c43b8eaf708315bb7da7e2f162e6b4
HamidKolli <gamog5756@gmail.com>
2021-03-31 08:56:15 +0200

    la strategie tourner

--- src/controller/strategies.py
+++ src/controller/strategies.py
@@ -37,6 +37,7 @@ class Avancer(Strategie):
             self.robot.MOTOR_LEFT + self.robot.MOTOR_RIGHT, 0)
         self.robot.set_motor_dps(
             self.robot.MOTOR_LEFT + self.robot.MOTOR_RIGHT, 0)
+        self.robot.servo_rotate(90)
 
     def run(self):
 
@@ -66,21 +67,26 @@ class Avancer(Strategie):
 
 class Tourner(Strategie):
 
-    def __init__(self, robot, angle, orientation):
+    GAUCHE = 1
+    DROITE = 0
+    def __init__(self, robot, angle, orientation,vitesse):
         super().__init__(robot)
         self.angle = angle
         self.angle_parcouru = 0
         self.orientation = orientation
+        self.vitesse = vitesse
+        self.distance_a_parcourire = 2 * PI * robot.WHEEL_BASE_WIDTH * angle / 360
+        self.distance_parcouru = 0
 
     def start(self):
         super().start()
         self.robot.offset_motor_encoder(
             self.robot.MOTOR_LEFT + self.robot.MOTOR_RIGHT, 0)
 
-        if self.orientation == 0:
+        if self.orientation == self.GAUCHE:
             self.robot.set_motor_dps(self.robot.MOTOR_LEFT,  0)
             self.robot.set_motor_dps(self.robot.MOTOR_RIGHT, self.angle)
-        else:
+        elif self.orientation == self.DROITE:
             self.robot.set_motor_dps(self.robot.MOTOR_LEFT,  self.angle)
             self.robot.set_motor_dps(self.robot.MOTOR_RIGHT, 0)
 
@@ -94,43 +100,49 @@ class Tourner(Strategie):
 
         # Calcule de l'angle_parcouru
 
-        if self.robot.get_distance() <= 5:
+        if self.orientation == self.GAUCHE:
+            diff = self.robot.get_motor_position()[0]
+        elif self.orientation == self.DROITE:
+            diff = self.robot.get_motor_position()[1]
+        
+
+        k = diff // 360
+        r = diff % 360
+
+        self.distance_parcouru += k * self.robot.WHEEL_CIRCUMFERENCE + \
+            (r * self.robot.WHEEL_CIRCUMFERENCE) / 360
+       
+        if self.robot.get_distance() <= 5 and self.distance_parcouru != self.distance_a_parcourire:
             self.robot.stop()
             self.stop()
             print("Arret de tourner")
             return
 
-        if self.orientation == 0:
+        if self.orientation == self.GAUCHE:
             self.robot.set_motor_dps(self.robot.MOTOR_LEFT,  0)
-            self.robot.set_motor_dps(self.robot.MOTOR_RIGHT, self.angle)
-        else:
-            self.robot.set_motor_dps(self.robot.MOTOR_LEFT,  self.angle)
+            self.robot.set_motor_dps(self.robot.MOTOR_RIGHT, self.vitesse)
+        elif self.orientation == self.GAUCHE:
+            self.robot.set_motor_dps(self.robot.MOTOR_LEFT,  self.vitesse)
             self.robot.set_motor_dps(self.robot.MOTOR_RIGHT, 0)
 
 
-"""
+
 class Carre(Strategie):
 
-    def __init__(...):
-        self.avancer = Avancer()
+    def __init__(self,robot):
+        self.avancer = Avancer(robot,)
         self.tourner = Tourner()
-        self.cur = 0
-        self.nb = 0 
-
-    def run
-
-    if self.is_stop:
-        return
-
-    if nb == NB_arret
         self.stop()
-        retunt
 
-    id self.cur = 0 
-    ---> a
+    def start(self):
+        super().start()
+        self.cur = 0
+        self.nb = 0
+    
+    def stop(self):
+        super().stop()
+        self.cur = -1
+        self.nb = 0
 
-    else --->
-
-
-    cur <-> switch
-"""
+    def run():
+        
