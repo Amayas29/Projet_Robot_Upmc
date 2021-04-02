1c56dec1a1a285d570d07eda5fe1a5e976c3e725
Replit user <replituser@example.com>
2021-04-02 07:52:18 +0000

    mise en place mode simu/irl avec config

--- src/main.py
+++ src/main.py
@@ -1,56 +1,55 @@
 from threading import Thread
 
-from controller.controleur import Controleur
-from controller.strategies import Carre
-# from irl.mockup import Robot2I013Mockup
-from robot2I013 import Robot2I013
-# from view.affichage import Affichage
 
-# from model.robot import Robot
-# from model.arene import Arene
-# from utils.tools import Point
+import configparser
+# protection du config
+config = configparser.ConfigParser()
 
-
-# import configparser
-# # protection du config
-# config = configparser.ConfigParser()
-
-# config.read('config.cfg')
-# if (float(config['Version']['config_version']) != 0.2):
-#     print("Config version non conforme")
-#     exit(1)
-# if (float(config['Robot']['distance_securite']) < 13.0):
-#     print("Erreur critique: la distance de sécurité est trop faible! (minimum 13.0)")
-#     exit(1)
-# mode = config['Robot'].getboolean('mode_simu')
-# if (mode):
-#     print("Simu on")
-# else:
-#     print("simu off")
+config.read('config.cfg')
+if (float(config['Version']['config_version']) != 0.2):
+  print("Config version non conforme")
+  exit(1)
+if (float(config['Robot']['distance_securite']) < 13.0):
+  print("Erreur critique: la distance de sécurité est trop faible! (minimum 13.0)")
+  exit(1)
+mode = config['Robot'].getboolean('mode_simu')
+if (mode):
+  print("Simu on")
+else:
+  print("simu off")
 # programe
 
-# arene = Arene()
-# centre = Point(100, 100)
-# robot = Robot(centre, arene)
-
-
-# arene.set_robot(robot)
-
-controleur = Controleur()
-robot = Robot2I013(controleur)
-carre = Carre(robot, 50, 300, 0)
-
-controleur.add_startegie(carre)
-controleur.select_startegie(0)
-
-# affichage = Affichage(arene)
 
 FPS = 60.0
 
-# thread_affichage = Thread(target=affichage.boucle, args=(FPS,))
-thread_controleur = Thread(target=controleur.boucle, args=(FPS,))
-# thread_modele = Thread(target=arene.boucle, args=(FPS,))
+if (mode):  #Mode Simu
+  from view.affichage import Affichage
+  from model.robot import Robot
+  from model.arene import Arene
+  from utils.tools import Point
+  arene = Arene()
+  centre = Point(100, 100)
+  robot = Robot(centre, arene)
+  arene.set_robot(robot)
+  affichage = Affichage(arene)
+  thread_affichage = Thread(target=affichage.boucle, args=(FPS,))
+  thread_modele = Thread(target=arene.boucle, args=(FPS,))
+  thread_modele.start()
+  thread_affichage.start()
+else:  #mode REEL
+  from controller.controleur import Controleur
+  from controller.strategies import Carre
+# from irl.mockup import Robot2I013Mockup
+  from robot2I013 import Robot2I013
+  controleur = Controleur()
+  robot = Robot2I013(controleur)
+  carre = Carre(robot, 50, 300, 0)
+  controleur.add_startegie(carre)
+  controleur.select_startegie(0)
+  thread_controleur = Thread(target=controleur.boucle, args=(FPS,))
+  thread_controleur.start()
 
-thread_controleur.start()
-# thread_modele.start()
-# thread_affichage.start()
+
+
+
+
