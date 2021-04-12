8220148b688f6a52d25f2ef99aecc7dfadcfb524
Replit user <replituser@example.com>
2021-04-12 08:56:59 +0000

    fichier tme

---
+++ src/tmesolo.py
@@ -0,0 +1,59 @@
+def q23() :
+    from threading import Thread
+    from controller.strategies import Tourner
+    from utils.config import Config
+    from controller.controleur import Controleur
+    from controller.strategies import Triangle
+
+    controleur = Controleur()
+
+    # protection du config
+    config = Config()
+
+    if (config.get_vers() != 0.4):
+        print("Config version non conforme")
+        print(config.get_vers())
+        exit(1)
+
+    if (config.get_dist_secu() < 13.0):
+        print("Erreur critique: la distance de sécurité est trop faible! (minimum 13.0)")
+        exit(1)
+
+    mode = config.get_mode()
+
+    FPS = config.get_fps()
+
+    if (mode):  # Mode Simu
+        print("Simu on")
+
+        from view.affichage import Affichage
+        from model.robot import Robot
+        from model.arene import Arene
+        from utils.tools import Point
+        from model.obstacles import Obstacle
+        import sys
+
+        arene = Arene()
+
+        robot = Robot(Point(230, 300), arene)
+        arene.set_robot(robot)
+
+        
+        affichage = Affichage(arene)
+
+        strat = Triangle(robot, 100, 300, 1)
+
+        controleur.add_startegie(strat)
+        controleur.select_startegie(0)
+
+        thread_controleur = Thread(target=controleur.boucle, args=(FPS,))
+        thread_modele = Thread(target=arene.boucle, args=(FPS,))
+        thread_affichage = Thread(target=affichage.boucle, args=(FPS,))
+
+        thread_controleur.start()
+        thread_modele.start()
+        thread_affichage.start()
+
+
+q23()
+
