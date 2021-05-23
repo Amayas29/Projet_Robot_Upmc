f64b4f7ed275fa435333e9423607209697e46830
Amayas29 <amayas29sd@gmail.com>
2021-05-23 15:42:15 +0200

    Commentaire pour imageloader et le mockup

--- src/irl/imageloader.py
+++ src/irl/imageloader.py
@@ -2,6 +2,9 @@ from time import sleep
 
 
 class ImageLoader:
+    """
+        Classe qui caputre constament l'image du robot et permet de reccupere l'image à un instant donnée
+    """
 
     def __init__(self, robot):
         self.robot = robot
@@ -9,13 +12,24 @@ class ImageLoader:
         self.run = True
 
     def boucle(self, fps):
-
+        """
+        float -> void
+        La boucle de capture des images dans le thread
+        """
         while self.run:
             self.image = self.robot.get_image()
             sleep(1./fps)
 
     def stop(self):
+        """
+        void -> void
+        Permet d'arreter la boucle du thread
+        """
         self.run = False
 
     def get_image(self):
+        """
+        void -> Image
+        Retourne l'image capturée
+        """
         return self.image
