22b95f6342e5e919c3fd8c53f6b9e11db05cdfd7
Amayas29 <amayas29sd@gmail.com>
2021-05-23 16:19:35 +0200

    Commentaire obstacles

--- src/model/obstacles.py
+++ src/model/obstacles.py
@@ -2,12 +2,20 @@ from utils.tools import Segment
 
 
 class Obstacle:
+    """
+        Les obstacles de la simulation
+
+        Un obstacle est un segment de droite
+    """
 
     def __init__(self, src, dest):
         self.segment = Segment(src, dest)
 
 
 class Balise(Obstacle):
+    """
+        La balise de la simualtion est un type d'obstacle
+    """
 
     def __init__(self, src, dest):
         super().__init__(src, dest)
